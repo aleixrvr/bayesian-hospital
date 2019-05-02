efaf6e18d4df19efb67fa6e68aed0ce3732f4fde
Aleix Ruiz de villa <aleixrvr@gmail.com>
2019-05-02 22:54:26 +0200

    BQ connection example

---
+++ BQ connection example.R
@@ -0,0 +1,26 @@
+library("bigrquery")
+library(DBI)
+library(dplyr)
+library(data.table)
+
+Sys.setenv(BIGQUERY_TEST_PROJECT="bgse-dsc")
+billing <- bq_test_project()
+
+con <- dbConnect(
+  bigrquery::bigquery(),
+  project = "bgse-dsc",
+  dataset = "MIMIC3_V1_4",
+  billing = billing
+)
+
+dbListTables(con)
+
+admissions <- tbl(con, "ADMISSIONS")
+admissions <- as.data.table(admissions)
+
+
+sql <- "select SUBJECT_ID
+from `MIMIC3_V1_4.ADMISSIONS` 
+limit 100"
+res <- dbGetQuery(con, sql, n = 10)
+
