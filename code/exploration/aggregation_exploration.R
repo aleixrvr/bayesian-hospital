c874b07ba9dd9ba3762aad9fa1bd0cec974b6d63
MaxZeb <36792608+MaxZeb@users.noreply.github.com>
2019-05-22 23:16:09 +0200

    small changes exploration

--- code/exploration/aggregation_exploration.R
+++ code/exploration/aggregation_exploration.R
@@ -1,20 +1,19 @@
-# min date: 2100-06-07
-# max date: 2209-08-07
-source("code/utils/stats_funciton.R")
-require(ggplot2)
+source("code/utils/stats_function.R") 
+require("tidyverse")
+min_date <- "2100-06-07"
+max_date <- "2209-08-07"
 
 ## How much staff is present /treatments given each day over the years on average
-dist_day <- calc_stats(window_start="2100-06-07",window_end="2209-08-07", 
-                           time_aggr_1 = "ISOYEAR", time_aggr_2 = "DAYOFYEAR", unit_aggr = T)
+dist_day <- calc_stats(min_date, max_date, time_aggregation = c("DATE"))
 dist_day[, .(avg_staff = mean(staff)), by=CURR_CAREUNIT]
-ggplot(dist_day, aes(CURR_CAREUNIT,staff)) +  geom_boxplot() + theme_bw() +
-    labs(x="Care Unit", y= "Number of Staff", title="Distribution of Staff")
-ggplot(dist_day, aes(CURR_CAREUNIT,treatments)) +  geom_boxplot() + theme_bw() +
-    labs(x="Care Unit", y= "Number of Treatments", title="Distribution of Treatments")
+ggplot(dist_day, aes(CURR_CAREUNIT,staff)) +  geom_boxplot() + theme_bw() + labs(x="Care Unit", y= "Number of Staff", title="Distribution of Staff")
+ggplot(dist_day, aes(CURR_CAREUNIT,treatments)) +  geom_boxplot() + theme_bw() + labs(x="Care Unit", y= "Number of Treatments", title="Distribution of Treatments")
 
 ## How much staff is present on average
-dist_mwd <- calc_stats(window_start="2101-01-01",window_end="2208-12-31", 
-                       time_aggr_1 = "MONTH", time_aggr_2 = "DAYOFWEEK", unit_aggr = T)
-dist_mwd[, .(avg_staff = mean(staff)), by=CURR_CAREUNIT]
-ggplot(dist_mwd, aes(CURR_CAREUNIT,staff)) +  geom_boxplot() + theme_bw() +
-    labs(x="Care Unit", y= "Number of Staff", title="Distribution of Staff on each Day")
+dist_mwd <- calc_stats(min_date, max_date, time_aggregation = c("ISOYEAR","MONTH"))
+dist_mwd[, newdate := paste0(ISOYEAR,MONTH)] %>% 
+    ggplot(aes(newdate, avg_los)) + geom_line() + facet_grid(CURR_CAREUNIT~.)
+
+
+
+
