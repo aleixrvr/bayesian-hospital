c47cb81fd16eb4f76cb6ecacb2d04ed27aad51d4
MaxZeb <36792608+MaxZeb@users.noreply.github.com>
2019-05-22 21:51:17 +0200

    update of the stats_calc function

--- code/utils/stats_function.R
+++ code/utils/stats_function.R
@@ -15,62 +15,41 @@ con <- dbConnect(
 dbListTables(con)
 
 # Function ====
-calc_stats <- function(window_start, window_end, time_aggr_1 = "TOTAL", time_aggr_2 = "NONE", unit_aggr = T){
-    if(unit_aggr){
-        uorcg <- "CURR_CAREUNIT"
+calc_stats <- function(window_start, window_end, time_shift = "CHARTTIME", 
+                       time_aggregation = "TOTAL", unit_aggregation = T){
+    # check input
+    allowed_shifts <- c("CHARTTIME", "CHARTTIME_SHIFT_1", "CHARTTIME_SHIFT_2", "CHARTTIME_SHIFT_3","CHARTTIME_SHIFT_4", "CHARTTIME_SHIFT_5")
+    if (! time_shift %in% allowed_shifts){
+        stop("Chart Time must be of the following: CHARTTIME, CHARTTIME_SHIFT_1, CHARTTIME_SHIFT_2, CHARTTIME_SHIFT_3, CHARTTIME_SHIFT_4, CHARTTIME_SHIFT_5")
+    }
+    allowed_times <- c("TOTAL", "HOUR", "DAYOFYEAR", "DAYOFWEEK", "ISOWEEK", "MONTH", "QUARTER", "ISOYEAR", "DATE")
+    if(sum(time_aggregation %in% allowed_times) != length(time_aggregation)){
+        stop("Time Aggregation must be of the following: TOTAL, HOUR, DAYOFYEAR, DAYOFWEEK, ISOWEEK, MONTH, QUARTER, ISOYEAR, DATE")
+    }
+    # aggregation levels
+    unit_or_category <- ifelse(unit_aggregation, "CURR_CAREUNIT", "CATEGORY_GROUP")
+    if(time_aggregation =="TOTAL"){ 
+        time_aggs <- "" 
+        times <- ""
     }
     else{
-        uorcg <- "CATEGORY_GROUP"
+        time_aggs <- paste(",", paste0(time_aggregation, collapse = ", "))
+        times <- paste(paste("EXTRACT(",time_aggregation, "FROM", time_shift, ") AS", time_aggregation, collapse = ", "), ",")
     }
-    if(! time_aggr_1 %in% c("TOTAL", "HOUR", "DAYOFYEAR", "DAYOFWEEK", "ISOWEEK", "MONTH", 
-                          "QUARTER", "ISOYEAR")){
-        stop("Time Aggregation must be of the following: 
-             TOTAL, HOUR, DAYOFYEAR, DAYOFWEEK, ISOWEEK, MONTH, QUARTER, ISOYEAR")
-    }
-    if(! time_aggr_2 %in% c("TOTAL", "HOUR", "DAYOFYEAR", "DAYOFWEEK", "ISOWEEK", "MONTH", 
-                            "QUARTER", "ISOYEAR", "NONE")){
-        stop("Time Aggregation must be of the following: 
-             TOTAL, HOUR, DAYOFYEAR, DAYOFWEEK, ISOWEEK, MONTH, QUARTER, ISOYEAR, NONE")
-    }
-    if(time_aggr_2 != "NONE"){
-        time_aggr <- paste0(time_aggr_1,", " , time_aggr_2)
-        time_features <- paste0("EXTRACT(",time_aggr_1, " FROM CHARTTIME) AS ", time_aggr_1,", ", 
-                                "EXTRACT(",time_aggr_2, " FROM CHARTTIME) AS ", time_aggr_2,", ")
-    }
-    else{
-        time_aggr <- time_aggr_1
-        time_features <- paste0("EXTRACT(",time_aggr, " FROM CHARTTIME) AS ", time_aggr,", ")
-    }
-    if(time_aggr == "TOTAL"){
-        sql <- paste0("SELECT ", 
-                      "DISTINCT ", uorcg, ", ", 
-                      "COUNT(DISTINCT ITEMID) AS treatments, COUNT(DISTINCT SUBJECT_ID) AS patients, ", 
-                      "COUNT(DISTINCT CGID) AS staff, AVG(LOS) AS avg_los ", 
-                      "FROM `MIMIC3_V1_4.CHARTEVENTS_DEPTS_CATS` ", 
-                      "WHERE DATE(CHARTTIME) >= '", window_start, "' ",
-                      "AND DATE(CHARTTIME) <= '" , window_end, "' ",
-                      "GROUP BY ", uorcg, " ",
-                      "ORDER BY ", uorcg, " ", ";")
-    }
-    else {
-        sql <- paste0("SELECT ", 
-                      "DISTINCT ", uorcg, ", ", 
-                      time_features,
-                      "COUNT(DISTINCT ITEMID) AS treatments, COUNT(DISTINCT SUBJECT_ID) AS patients, ", 
-                      "COUNT(DISTINCT CGID) AS staff, AVG(LOS) AS avg_los ", 
-                      "FROM `MIMIC3_V1_4.CHARTEVENTS_DEPTS_CATS` ", 
-                      "WHERE DATE(CHARTTIME) >= '", window_start, "' ",
-                      "AND DATE(CHARTTIME) <= '" , window_end, "' ",
-                      "GROUP BY ", uorcg, ", ", time_aggr, " ",
-                      "ORDER BY ", uorcg, ", ", time_aggr, ";")
-    }
+    # building the query
+    sql <- paste("SELECT", "DISTINCT", unit_or_category, ",", times, "COUNT(DISTINCT ITEMID) AS treatments,", "COUNT(DISTINCT SUBJECT_ID) AS patients,", "COUNT(DISTINCT CGID) AS staff, AVG(LOS) AS avg_los", 
+                 "FROM `MIMIC3_V1_4.CHARTEVENTS_DEPTS_CATS_TS`", 
+                 "WHERE DATE(", time_shift, ") >=", paste0("'", window_start, "'"), "AND DATE(", time_shift, ") <=", paste0("'" , window_end, "'"),
+                 "GROUP BY ", unit_or_category, time_aggs,
+                 "ORDER BY ", unit_or_category, time_aggs, ";")
+    # execute the query and return results
     statistics <- dbGetQuery(con, sql)
-    as.data.table(statistics)
+    return(as.data.table(statistics))
 }
 
 # Check Function ====
 #check_intervals <- c("TOTAL", "HOUR", "DAYOFYEAR", "DAYOFWEEK", "ISOWEEK", "MONTH", "QUARTER", "ISOYEAR", "OTHER")
-#for(i in check_intervals) print(calc_stats(window_start="2100-03-01",window_end="2108-03-30", time_aggr_1 = i, unit_aggr = F))
+#for(i in check_intervals) print(calc_stats(window_start="2100-03-01",window_end="2108-03-30", time_aggregation = i, unit_aggr = F))
 
 
 
