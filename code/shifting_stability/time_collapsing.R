b47bfa2a03ab88f88bb31a965b87fb2748b4ca80
reidfalconer <reidfalcon@gmail.com>
2019-06-18 23:14:34 +0200

    update `time_collapsing.R` script

--- code/shifting_stability/time_collapsing.R
+++ code/shifting_stability/time_collapsing.R
@@ -48,16 +48,38 @@ dbListTables(con)
 
 # TIME SHIFT GENERATION ====
 
-sql <- paste0("SELECT * ",
-              "FROM MIMIC3_V1_4.ICUSTAYS AS ICUSTAYS")
+sql <- paste0("
+SELECT
+  *
+  FROM (
+    SELECT
+    *
+  FROM
+    `MIMIC3_V1_4.ICUSTAYS`) AS ICUSTAYS
+LEFT JOIN (
+  SELECT
+    ICUSTAY_ID,
+    INTIME AS INTIME_TRANS,
+    OUTTIME AS OUTTIME_TRANS,
+    LOS AS LOS_TRANS
+  FROM
+    `MIMIC3_V1_4.TRANSFERS`) AS TRANS
+USING
+  (ICUSTAY_ID)
+WHERE
+ICUSTAYS.INTIME <= TRANS.INTIME_TRANS
+AND TRANS.OUTTIME_TRANS <= ICUSTAYS.OUTTIME
+              ")
 
 time_collapse <- dbGetQuery(con, sql)
+glimpse(time_collapse)
 
 collapse_times <- function(source = "metavision") {
   input = 1000:3000
   multiple_of_7 = (input %% 7) == 0
   input = input[multiple_of_7]
   df <- time_collapse
+  set.seed(2019)
   df <- time_collapse %>% 
     mutate(SHIFT_1 = sample(input, size = nrow(df), replace = TRUE),
            SHIFT_2 = sample(input, size = nrow(df), replace = TRUE),
@@ -68,8 +90,12 @@ collapse_times <- function(source = "metavision") {
            INTIME_SHIFT_2 = INTIME + days(SHIFT_2),
            INTIME_SHIFT_3 = INTIME + days(SHIFT_3),
            INTIME_SHIFT_4 = INTIME + days(SHIFT_4),
-           INTIME_SHIFT_5 = INTIME + days(SHIFT_5))
-  
+           INTIME_SHIFT_5 = INTIME + days(SHIFT_5),
+           INTIME_TRANS_SHIFT_1 = INTIME_TRANS + days(SHIFT_1),
+           INTIME_TRANS_SHIFT_2 = INTIME_TRANS + days(SHIFT_2),
+           INTIME_TRANS_SHIFT_3 = INTIME_TRANS + days(SHIFT_3),
+           INTIME_TRANS_SHIFT_4 = INTIME_TRANS + days(SHIFT_4),
+           INTIME_TRANS_SHIFT_5 = INTIME_TRANS + days(SHIFT_5))
   if(source == "carevue") {
     years <- 7
     start_date = "2001-01-01"
@@ -86,7 +112,9 @@ collapse_times <- function(source = "metavision") {
   }
   df_collapsed <- df
   df_time <-df %>% 
-    select(INTIME, INTIME_SHIFT_1, INTIME_SHIFT_2, INTIME_SHIFT_3, INTIME_SHIFT_4, INTIME_SHIFT_5)
+    select(INTIME, INTIME_SHIFT_1, INTIME_SHIFT_2, INTIME_SHIFT_3, INTIME_SHIFT_4, INTIME_SHIFT_5,
+           INTIME_TRANS, INTIME_TRANS_SHIFT_1, INTIME_TRANS_SHIFT_2, INTIME_TRANS_SHIFT_3, 
+           INTIME_TRANS_SHIFT_4, INTIME_TRANS_SHIFT_5)
   for (time in colnames(df_time)) {
   INTIME_a <- df[[time]] - min(df[[time]])
   INTIME_a <- lubridate::as.period(INTIME_a, unit = "days")
@@ -102,10 +130,11 @@ collapse_times <- function(source = "metavision") {
                      lubridate::hour(df[[time]]))
   INTIME_COLLAPSED_date <- ymd_hms(INTIME_COLLAPSED_date, truncated = 3, tz = "Etc/GMT+5") + lubridate::hms(time_of_day)
   print(identical(lubridate::wday(INTIME_COLLAPSED_date), lubridate::wday(df$INTIME)))
+  print(identical(lubridate::wday(INTIME_COLLAPSED_date), lubridate::wday(df$INTIME_TRANS)))
   intime_var_name <- paste0(time,"_COLLAPSED")
   outtime_var_name <- paste0("OUTTIME",substring(time, 7),"_COLLAPSED")
   df_collapsed[[intime_var_name]] <- with(df_collapsed, INTIME_COLLAPSED_date)
-  df_collapsed[[outtime_var_name]] <- with(df_collapsed, INTIME_COLLAPSED_date + lubridate::as.difftime(df$LOS, units = "days"))
+  df_collapsed[[outtime_var_name]] <- with(df_collapsed, INTIME_COLLAPSED_date + lubridate::as.difftime(df$LOS_TRANS, units = "days"))
   }
   return(df_collapsed)
 }
@@ -116,8 +145,11 @@ metavision <- collapse_times(source = "metavision")
 # nrow(carvue) + nrow(metavision)
 # nrow(all)
 
-df_collapsed <- rbind(carvue, metavision)
-#glimpse(df_collapsed)
+df_collapsed <- rbind(carvue, metavision) %>% 
+  select(-c(starts_with("OUTTIME_SHIFT")))
+ 
+
+glimpse(df_collapsed)
 
 # write table to BigQuery
-#dbWriteTable(con, name = "ICUSTAYS_COLLAPSED", df_collapsed, row.names = TRUE, overwrite = TRUE)
+dbWriteTable(con, name = "ICUSTAYS_TRANS_COLLAPSED", df_collapsed, row.names = TRUE, overwrite = TRUE)
