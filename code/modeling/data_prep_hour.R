6d800131e239e61df3e2e4ef55deaa6589747933
reidfalconer <reidfalcon@gmail.com>
2019-06-20 12:34:13 +0200

    add hour scripts

---
+++ code/modeling/data_prep_hour.R
@@ -0,0 +1,126 @@
+#rm(list=ls())
+
+# Library ====
+library(bigrquery)
+library(DBI)
+library(data.table)
+library(lubridate)
+library(ggplot2)
+library(dplyr)
+
+# functions ====
+
+lagpad <- function(x, k = 1) {
+  if (!is.vector(x))
+    stop('x must be a vector')
+  if (!is.numeric(x))
+    stop('x must be numeric')
+  if (!is.numeric(k))
+    stop('k must be numeric')
+  if (1 != length(k))
+    stop('k must be a single number')
+  c(rep(NA, k), x)[1 : length(x)]
+}
+
+# Connection to BigQuery ====
+Sys.setenv(BIGQUERY_TEST_PROJECT="bgse-dsc")
+billing <- bq_test_project()
+con <- dbConnect(
+    bigrquery::bigquery(),
+    project = "bgse-dsc",
+    dataset = "MIMIC3_V1_4",
+    billing = billing
+)
+
+
+# Raw Data ====
+skeleton_sql <- paste("
+SELECT 
+    EXTRACT(DATE FROM CHARTTIME_COLLAPSED) AS CHART_DATE,
+    EXTRACT(HOUR FROM CHARTTIME_COLLAPSED) AS CHART_HOUR,
+    CURR_CAREUNIT AS CURR_UNIT, 
+    COUNT(DISTINCT CGID) AS STAFF,
+    AVG(LOS_TRANS) AS AVG_LOS
+FROM `MIMIC3_V1_4.CHARTEVENTS_DEPTS_CATS_TS_COLLAPSED_NEW`
+GROUP BY CHART_DATE, CHART_HOUR, CURR_UNIT
+ORDER BY CHART_DATE, CHART_HOUR
+")
+skeleton <- dbGetQuery(con, skeleton_sql)
+skeleton <- as.data.table(skeleton)
+inflow_sql <- paste("
+SELECT 
+    ICUSTAY_ID,
+    CURR_CAREUNIT AS CURR_UNIT,
+    IFNULL(ANY_VALUE(PREV_CAREUNIT), ", "'OUT'" , ") AS PREV_CAREUNIT,
+    EXTRACT(DATE FROM ANY_VALUE(INTIME_TRANS_COLLAPSED)) AS CHART_DATE,
+    EXTRACT(HOUR FROM ANY_VALUE(INTIME_TRANS_COLLAPSED)) AS CHART_HOUR
+FROM `MIMIC3_V1_4.CHARTEVENTS_DEPTS_CATS_TS_COLLAPSED_NEW`
+GROUP BY ICUSTAY_ID, CURR_UNIT
+ORDER BY CHART_DATE, CHART_HOUR
+")
+inflow <- dbGetQuery(con, inflow_sql)
+inflow <- as.data.table(inflow)
+outflow_sql <- paste("
+SELECT ICUSTAY_ID, CHART_DATE, CHART_HOUR, CURR_UNIT, IFNULL(NEXT_CAREUNIT,'OUT') AS NEXT_CAREUNIT
+FROM 
+(   SELECT 
+        ICUSTAY_ID,
+        CURR_CAREUNIT AS CURR_UNIT, 
+        ANY_VALUE(EXTRACT(DATE FROM OUTTIME_TRANS_COLLAPSED)) AS CHART_DATE,
+        ANY_VALUE(EXTRACT(HOUR FROM OUTTIME_TRANS_COLLAPSED)) AS CHART_HOUR
+    FROM `MIMIC3_V1_4.CHARTEVENTS_DEPTS_CATS_TS_COLLAPSED_FINAL` 
+    GROUP BY ICUSTAY_ID, CURR_UNIT) AS L
+LEFT JOIN 
+(   SELECT 
+        ICUSTAY_ID,
+        CURR_CAREUNIT AS NEXT_CAREUNIT, 
+        ANY_VALUE(EXTRACT(DATE FROM INTIME_TRANS_COLLAPSED)) AS CHART_DATE,
+        ANY_VALUE(EXTRACT(HOUR FROM INTIME_TRANS_COLLAPSED)) AS CHART_HOUR
+    FROM `MIMIC3_V1_4.CHARTEVENTS_DEPTS_CATS_TS_COLLAPSED_FINAL` 
+    GROUP BY ICUSTAY_ID, NEXT_CAREUNIT) AS R
+USING(ICUSTAY_ID, CHART_DATE, CHART_HOUR)
+ORDER BY CHART_DATE, CHART_HOUR
+")
+outflow <- dbGetQuery(con, outflow_sql)
+outflow <- as.data.table(outflow)
+
+# Make detailed Flows ====
+detailed_inflow <- inflow[,.("from_OUT"=sum(PREV_CAREUNIT=="OUT"), 
+                             "from_NWARD"=sum(PREV_CAREUNIT=="NWARD"),
+                             "from_NICU"=sum(PREV_CAREUNIT=="NICU"),
+                             "from_MICU"=sum(PREV_CAREUNIT=="MICU"),
+                             "from_TSICU"=sum(PREV_CAREUNIT=="TSICU"),
+                             "from_CSRU"=sum(PREV_CAREUNIT=="CSRU"),
+                             "from_SICU"=sum(PREV_CAREUNIT=="SICU"),
+                             "from_CCU"=sum(PREV_CAREUNIT=="CCU")), 
+                          by=list(CHART_DATE, CHART_HOUR,CURR_UNIT)]
+
+detailed_outflow <- outflow[,.("to_OUT"=sum(NEXT_CAREUNIT=="OUT"), 
+                               "to_NWARD"=sum(NEXT_CAREUNIT=="NWARD"),
+                               "to_NICU"=sum(NEXT_CAREUNIT=="NICU"),
+                               "to_MICU"=sum(NEXT_CAREUNIT=="MICU"),
+                               "to_TSICU"=sum(NEXT_CAREUNIT=="TSICU"),
+                               "to_CSRU"=sum(NEXT_CAREUNIT=="CSRU"),
+                               "to_SICU"=sum(NEXT_CAREUNIT=="SICU"),
+                               "to_CCU"=sum(NEXT_CAREUNIT=="CCU")), 
+                            by=list(CHART_DATE, CHART_HOUR,CURR_UNIT)]
+
+# Merge stuff ====
+flow_data <- skeleton  %>% 
+  left_join(y=detailed_inflow, by=c("CHART_DATE", "CHART_HOUR", "CURR_UNIT")) %>% 
+  left_join(y=detailed_outflow, by=c("CHART_DATE", "CHART_HOUR", "CURR_UNIT")) %>% 
+  as.data.table()
+
+# Cleaning Dataset ====
+in_cols <- c("from_OUT", "from_NWARD", "from_NICU", "from_MICU", 
+             "from_TSICU", "from_CSRU", "from_SICU", "from_CCU")
+out_cols <- c("to_OUT", "to_NWARD", "to_NICU", "to_MICU", "to_TSICU",
+              "to_CSRU", "to_SICU", "to_CCU")
+relabel_cols <- c(in_cols, out_cols)
+flow_data <- as.data.frame(flow_data)
+flow_data[relabel_cols][is.na(flow_data[relabel_cols])] <- 0 
+flow_data["INFLOW"] <- rowSums(flow_data[in_cols])
+flow_data["OUTFLOW"] <- rowSums(flow_data[out_cols])
+flow_data <- as.data.table(flow_data)
+
+
---
+++ code/modeling/models_hour.R
@@ -0,0 +1,148 @@
+library(stargazer)
+source("code/modeling/data_prep_hour.R")
+
+model_data <- flow_data %>% 
+  group_by(CURR_UNIT) %>% 
+  mutate_at(c("from_OUT" ,"from_NWARD" ,"from_NICU", "from_MICU", "from_TSICU" ,"from_CSRU" ,"from_SICU" ,"from_CCU" ), lagpad) %>% 
+  mutate(net_flow = OUTFLOW - INFLOW,
+         max_wait = 1/1,
+         waiting_time = 1/pmax(net_flow, max_wait),
+         l1_waiting_time = lagpad(waiting_time),
+         l2_waiting_time = lagpad(waiting_time, 2),
+         l_OUTFLOW = lagpad(OUTFLOW),
+         l_STAFF = lagpad(STAFF),
+         l_INFLOW = lagpad(INFLOW)) %>% 
+  select(-c(AVG_LOS)) %>% 
+  ungroup() 
+
+glimpse(model_data)
+
+CCU_flow <- model_data %>% 
+  filter(CURR_UNIT == "CCU")
+
+CSRU_flow <- model_data %>% 
+  filter(CURR_UNIT == "CSRU") 
+
+MICU_flow <- model_data %>% 
+  filter(CURR_UNIT == "MICU") 
+
+SICU_flow <- model_data %>% 
+  filter(CURR_UNIT == "SICU") 
+
+TSICU_flow <- model_data %>% 
+  filter(CURR_UNIT == "TSICU") 
+
+CCU_flow <- CCU_flow  %>% 
+  left_join(y = CSRU_flow %>% select(CHART_DATE, CHART_HOUR, l1_waiting_time, l2_waiting_time, l_STAFF, l_INFLOW), 
+            by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l1_waiting_time_csru = l1_waiting_time.y,
+         l2_waiting_time_csru = l2_waiting_time.y,
+         l_STAFF_csru = l_STAFF.y,
+         l_INFLOW_csru = l_INFLOW.y) %>% 
+  left_join(y = MICU_flow %>% select(CHART_DATE, CHART_HOUR, l1_waiting_time, l2_waiting_time, l_STAFF, l_INFLOW), 
+            by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l1_waiting_time_micu = l1_waiting_time,
+         l2_waiting_time_micu = l2_waiting_time,
+         l_STAFF_micu = l_STAFF,
+         l_INFLOW_micu = l_INFLOW) %>% 
+  left_join(y = SICU_flow %>% select(CHART_DATE, CHART_HOUR, l1_waiting_time, l2_waiting_time, l_STAFF, l_INFLOW), 
+            by =  c("CHART_DATE","CHART_HOUR")) %>% 
+  rename(l1_waiting_time_sicu = l1_waiting_time,
+         l2_waiting_time_sicu = l2_waiting_time,
+         l_STAFF_sicu = l_STAFF,
+         l_INFLOW_sicu = l_INFLOW) %>% 
+  left_join(y = TSICU_flow %>% select(CHART_DATE, CHART_HOUR, l1_waiting_time, l2_waiting_time, l_STAFF, l_INFLOW), 
+            by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l1_waiting_time_tsicu = l1_waiting_time,
+         l2_waiting_time_tsicu = l2_waiting_time,
+         l_STAFF_tsicu = l_STAFF,
+         l_INFLOW_tsicu = l_INFLOW,
+         l_STAFF = l_STAFF.x,
+         l_INFLOW = l_INFLOW.x,
+         l1_waiting_time = l1_waiting_time.x,
+         l2_waiting_time = l2_waiting_time.x)
+
+CSRU_flow <- CSRU_flow  %>% 
+  left_join(y = CCU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_ccu = l_waiting_time.y) %>% 
+  left_join(y = MICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_micu = l_waiting_time) %>% 
+  left_join(y = SICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_sicu = l_waiting_time) %>% 
+  left_join(y = TSICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_tsicu = l_waiting_time,
+         l_waiting_time = l_waiting_time.x)
+
+MICU_flow <- MICU_flow  %>% 
+  left_join(y = CCU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_ccu = l_waiting_time.y) %>% 
+  left_join(y = CSRU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_csru = l_waiting_time) %>% 
+  left_join(y = SICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>%  
+  rename(l_waiting_time_sicu = l_waiting_time) %>% 
+  left_join(y = TSICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>%  
+  rename(l_waiting_time_tsicu = l_waiting_time,
+         l_waiting_time = l_waiting_time.x)
+
+SICU_flow <- SICU_flow  %>% 
+  left_join(y = CCU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_ccu = l_waiting_time.y) %>% 
+  left_join(y = CSRU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_csru = l_waiting_time) %>% 
+  left_join(y = MICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_micu = l_waiting_time) %>% 
+  left_join(y = TSICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_tsicu = l_waiting_time,
+         l_waiting_time = l_waiting_time.x)
+
+TSICU_flow <- TSICU_flow  %>% 
+  left_join(y = CCU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_ccu = l_waiting_time.y) %>% 
+  left_join(y = CSRU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_crsu = l_waiting_time) %>% 
+  left_join(y = MICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_micu = l_waiting_time) %>% 
+  left_join(y = SICU_flow %>% select(CHART_DATE, l_waiting_time, CHART_HOUR), by =  c("CHART_DATE", "CHART_HOUR")) %>% 
+  rename(l_waiting_time_sicu = l_waiting_time,
+         l_waiting_time = l_waiting_time.x)
+
+
+## OLS models
+
+inflow_ccu_mod <- lm(INFLOW ~ ., CCU_flow %>% select(INFLOW, starts_with("from_"), -from_CCU))
+outflow_ccu_mod <- lm(OUTFLOW ~ ., CCU_flow %>% select(OUTFLOW, STAFF, starts_with("l_waiting_time_")))
+
+inflow_csru_mod <- lm(INFLOW ~ ., CSRU_flow %>% select(INFLOW, starts_with("from_"), -from_CSRU))
+outflow_csru_mod <- lm(OUTFLOW ~ ., CSRU_flow %>% select(OUTFLOW, STAFF, starts_with("l_waiting_time_")))
+
+inflow_micu_mod <- lm(INFLOW ~ ., MICU_flow %>% select(INFLOW, starts_with("from_"), -from_MICU))
+outflow_micu_mod <-lm(OUTFLOW ~ ., MICU_flow %>% select(OUTFLOW, STAFF, starts_with("l_waiting_time_")))
+
+inflow_sicu_mod <- lm(INFLOW ~ ., SICU_flow %>% select(INFLOW, starts_with("from_"), -from_SICU))
+outflow_sicu_mod <-lm(OUTFLOW ~ ., SICU_flow %>% select(OUTFLOW, STAFF, starts_with("l_waiting_time_")))
+
+inflow_tsicu_mod <- lm(INFLOW ~ ., TSICU_flow %>% select(INFLOW, starts_with("from_"), -from_TSICU))
+outflow_tsiu_mod <-lm(OUTFLOW ~ ., TSICU_flow %>% select(OUTFLOW, STAFF, starts_with("l_waiting_time_")))
+
+
+summary(outflow_tsiu_mod)
+
+stargazer(inflow_ccu_mod, inflow_csru_mod, inflow_micu_mod, inflow_sicu_mod, inflow_tsicu_mod, title="Results", align=TRUE, type = "latex")
+stargazer(outflow_ccu_mod, outflow_csru_mod, outflow_micu_mod, outflow_sicu_mod, inflow_tsicu_mod, title="Results", align=TRUE, type = "latex")
+
+
+ggplotRegression <- function (fit) {
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)))
+}
+
+
+ggplotRegression(outflow_micu_mod)
+
+
