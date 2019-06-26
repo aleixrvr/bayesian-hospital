662abec34c3519b4937a846e5ccebab3d5a3b618
Aleix Ruiz de villa <aleixrvr@gmail.com>
2019-06-26 19:43:07 +0200

    include downstream calculation

--- code/modeling/waiting_curves_updated.R
+++ code/modeling/waiting_curves_updated.R
@@ -2,9 +2,12 @@
 #source("code/modeling/data_prep_updated_hour.R")
 source("code/modeling/data_prep_updated_other_aggregation.R")
 #source("code/modeling/data_prep_updated_day.R")
-ls()
+library(ggplot2)
+# ls()
 
+# unit_flow <- unit_flow_hour
 unit_flow <- unit_flow_shift
+# unit_flow <- unit_flow_day
 # Modelling ====
 units <- names(unit_flow)
 inflow_models <- list()
@@ -24,7 +27,7 @@ rm(list=c("i", "to_model_outflow", "to_model_inflow","units"))
 outflow_models
 
 # Waiting times curves ====
-waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(0,100), w_max = 3){
+waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(1,100), w_max = 3){
     # set units in the ICU to be examined
     all_units <- names(unit_flow_data)
     units <- all_units[all_units != unit]
@@ -32,8 +35,10 @@ waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(0,100), w_max
     unit_flow_data[[unit]] %>% 
         select(starts_with("l_STAFF_"), starts_with("l_INFLOW_"), 
                starts_with("l2_waiting_time_"), 
-               l2_waiting_time, INFLOW) %>% as.data.table() -> pred_data
+               l2_waiting_time, INFLOW, STAFF) %>% as.data.table() -> pred_data
     pred_data <- pred_data[complete.cases(pred_data)]
+    staff_data <- pred_data$STAFF
+    pred_data <- select(pred_data, -STAFF)
     colnames(pred_data)[colnames(pred_data) == "l2_waiting_time"] <- paste0("l2_waiting_time_",unit)
     colnames(pred_data) <- gsub("2","1",colnames(pred_data))
     # estimate outflows for downstream units
@@ -46,42 +51,54 @@ waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(0,100), w_max
         colnames(pred_data) <- orig_cols
     }
     # estimate waiting time curves by varying staff in the unit of interest 
-    to_plot <- data.frame(ressources=integer(), waiting_time=integer())#, do_unit=character())
+    to_plot <- data.frame(ressources=integer(), waiting_time=integer(), unit=character())#, do_unit=character())
     for(ressources in r[1]:r[2]){
         df <- data.frame(matrix(unlist(pred_outflows), ncol=length(pred_outflows), byrow=FALSE))
         df <- cbind(ressources, df)
         colnames(df) <- c("STAFF", paste0("l1_waiting_time_",names(pred_outflows)))
-        #to_plot[ressources, "do_unit"] <- unit
-        to_plot[ressources, "ressources"] <- ressources
-        to_plot[ressources, "waiting_time"] <- 
-            mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - pred_data$INFLOW), w_max))
+        waiting_time <- mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - pred_data$INFLOW), w_max))
+        to_plot <- rbind(to_plot, data.frame(ressources, waiting_time, unit))
     }
-    # estimate waiting time curves by varying staff in the downstream units
-    # for(i in units){
-    #   orig_cols <- colnames(pred_data)
-    #   colnames(pred_data)[colnames(pred_data) == paste0("l_STAFF_",i)] <- "STAFF"
-    #   inflow <- as.data.frame(pred_data)[paste0("l_INFLOW_",i)]
-    #   pred_outflows[[i]] <- 1 / theta_thres(predict(outflow_models[[i]],  pred_data) - inflow, w_max)
-    #   colnames(pred_data) <- orig_cols
-    # }
-    # for(ressources in r[1]:r[2]){
-    #   df <- data.frame(matrix(unlist(pred_outflows), ncol=length(pred_outflows), byrow=FALSE))
-    #   df <- cbind(unit_flow[[unit]]$STAFF, df)
-    #   colnames(df) <- c("STAFF", paste0("l1_waiting_time_",names(pred_outflows)))
-    #   to_plot[ressources, "do_unit"] <- unit
-    #   to_plot[ressources, "ressources"] <- ressources
-    #   to_plot[ressources, "waiting_time"] <-
-    #     mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - pred_data$INFLOW), w_max))
-    # }
-    # return all estimated waiting time curves
-    return(to_plot[r[1]:r[2],])
-}
-par(mfrow=c(3,2))
-for(i in names(unit_flow)){
-    see <- waiting_do(i, unit_flow, outflow_models)
-    plot(see$ressources, see$waiting_time,type = "l", col="tomato", lwd=2, 
-         main=paste("Waiting Curve", i), ylim=c(0,3))
     
+    # estimate waiting time curves by varying staff in the downstream units
+    for(downstream_unit in units){
+      pred_outflows_old <- pred_outflows[[downstream_unit]]
+      
+      for(ressources in r[1]:r[2]){
+      
+        orig_cols <- colnames(pred_data)
+        colnames(pred_data)[colnames(pred_data) == paste0("l_STAFF_", downstream_unit)] <- "STAFF"
+        pred_data[, "STAFF"] <- ressources
+        inflow <- as.data.frame(pred_data)[paste0("l_INFLOW_", downstream_unit)]
+        pred_outflows[[downstream_unit]] <- 1 / theta_thres(predict(outflow_models[[downstream_unit]],  pred_data) - inflow, w_max)
+        colnames(pred_data) <- orig_cols
+        
+        df <- data.frame(matrix(unlist(pred_outflows), ncol=length(pred_outflows), byrow=FALSE))
+        df <- cbind(staff_data, df)
+        colnames(df) <- c("STAFF", paste0("l1_waiting_time_",names(pred_outflows)))
+        waiting_time <- mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - pred_data$INFLOW), w_max))
+        to_plot <- rbind(to_plot, data.frame(ressources, waiting_time, unit=downstream_unit))
+      }
+      
+      pred_outflows[[downstream_unit]] <- pred_outflows_old
+    }
+
+    return(to_plot)
 }
-par(mfrow=c(1,1))
+
+# par(mfrow=c(3,2))
+# for(i in names(unit_flow)){
+#     see <- waiting_do(i, unit_flow, outflow_models)
+#     plot(see$ressources, see$waiting_time,type = "l", col="tomato", lwd=2, 
+#          main=paste("Waiting Curve", i), ylim=c(0,3))
+#     
+# }
+# par(mfrow=c(1,1))
+
+see <- waiting_do("CCU", unit_flow, outflow_models)
+ggplot(see, aes(ressources, waiting_time, group=unit, color=unit)) +
+  geom_line()
+
+# plot(see$ressources, see$waiting_time,type = "l", col="tomato", lwd=2)
+
 
