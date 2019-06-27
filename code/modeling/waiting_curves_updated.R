a8e4eaec263dab8be9f3f37e1dc26ef9f130d98e
MaxZeb <36792608+MaxZeb@users.noreply.github.com>
2019-06-27 09:56:51 +0200

    updated waiting curves

--- code/modeling/waiting_curves_updated.R
+++ code/modeling/waiting_curves_updated.R
@@ -37,7 +37,7 @@ rm(list=c("i", "to_model_outflow", "to_model_inflow","units"))
 outflow_models
 
 # Waiting times curves ====
-waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(1,100), w_max = 3){
+waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(1,100), w_max = 2){
     # set units in the ICU to be examined
     do_unit <- unit
     all_units <- names(unit_flow_data)
@@ -62,12 +62,14 @@ waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(1,100), w_max
         colnames(pred_data) <- orig_cols
     }
     # estimate waiting time curves by varying staff in the unit of interest 
-    to_plot <- data.frame(ressources=integer(), waiting_time=integer(), unit=character(), do_unit=character())
+    to_plot <- data.frame(ressources=integer(), waiting_time=integer(), unit=character(), 
+                          do_unit=character())
     for(ressources in r[1]:r[2]){
         df <- data.frame(matrix(unlist(pred_outflows), ncol=length(pred_outflows), byrow=FALSE))
         df <- cbind(ressources, df)
         colnames(df) <- c("STAFF", paste0("l1_waiting_time_",names(pred_outflows)))
-        waiting_time <- mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - pred_data$INFLOW), w_max))
+        waiting_time <- mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - 
+                                                            pred_data$INFLOW), w_max))
         to_plot <- rbind(to_plot, data.frame(ressources, waiting_time, unit, do_unit))
     }
     
@@ -81,13 +83,15 @@ waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(1,100), w_max
         colnames(pred_data)[colnames(pred_data) == paste0("l_STAFF_", downstream_unit)] <- "STAFF"
         pred_data[, "STAFF"] <- ressources
         inflow <- as.data.frame(pred_data)[paste0("l_INFLOW_", downstream_unit)]
-        pred_outflows[[downstream_unit]] <- 1 / theta_thres(predict(outflow_models[[downstream_unit]],  pred_data) - inflow, w_max)
+        pred_outflows[[downstream_unit]] <- 1 / theta_thres(predict(outflow_models[[downstream_unit]],  
+                                                                    pred_data) - inflow, w_max)
         colnames(pred_data) <- orig_cols
         
         df <- data.frame(matrix(unlist(pred_outflows), ncol=length(pred_outflows), byrow=FALSE))
         df <- cbind(staff_data, df)
         colnames(df) <- c("STAFF", paste0("l1_waiting_time_",names(pred_outflows)))
-        waiting_time <- mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - pred_data$INFLOW), w_max))
+        waiting_time <- mean(1 / theta_thres(data.frame(predict(outflow_models[[unit]], df) - 
+                                                            pred_data$INFLOW), w_max))
         to_plot <- rbind(to_plot, data.frame(ressources, waiting_time, unit=downstream_unit, do_unit))
       }
       
@@ -105,6 +109,7 @@ waiting_do <- function(unit, unit_flow_data, outflow_models, r = c(1,100), w_max
 #     
 # }
 # par(mfrow=c(1,1))
+# plot(see$ressources, see$waiting_time,type = "l", col="tomato", lwd=2)
 
 to_plot <- data.frame(ressources=integer(), waiting_time=integer(), unit=character(), do_unit=character())
 for(unit_name in names(unit_flow)){
@@ -112,11 +117,40 @@ for(unit_name in names(unit_flow)){
   to_plot <- rbind(to_plot, do_waiting)
 }
 
-
 ggplot(to_plot, aes(ressources, waiting_time, group=unit, color=unit)) +
-  geom_line() +
-  facet_grid(.~do_unit)
+    geom_line() +
+    facet_grid(.~do_unit)
 
-# plot(see$ressources, see$waiting_time,type = "l", col="tomato", lwd=2)
+
+colors <- c(I("#9ebcda"), "gray79", "gray54", "gray46", "gray25")
+par(cex.axis=.8, cex.lab=.9, cex.main=1.2)
+units <- as.character(unique(to_plot$unit))
+for(u in units){
+    see <- to_plot[to_plot$unit == u,]
+    unit_detail <- as.character(unique(see$unit))
+    other_units <- as.character(unique(see$do_unit))
+    other_units <- other_units[other_units != unit_detail]
+    
+    plot(see[see$do_unit == unit_detail,"ressources"], 
+         see[see$do_unit == unit_detail,"waiting_time"], 
+         col=colors[1], type = "l", lwd=4, 
+         ylab="Waiting Times", las=2, ylim=c(0,2.5),
+         xlab= "Ressources", frame.plot=FALSE, xaxt="n",
+         main=paste("Change in waiting times in the", unit_detail, "dependent", 
+                    "\n on staff and downstream units")) 
+    axis(1, at=c(0,25,50,75,100))
+    for(i in 1:length(other_units)){
+        lines(see[see$do_unit == other_units[i],"ressources"],
+              see[see$do_unit == other_units[i],"waiting_time"], 
+              lwd=2, col=colors[2:5][i])
+    }
+    lines(see[see$do_unit == unit_detail,"ressources"], 
+          see[see$do_unit == unit_detail,"waiting_time"], 
+          col=colors[1], type = "l", lwd=4)
+    legend("bottomleft", c(unit_detail, other_units), col = colors, lwd = 3, cex = .8, bty="n")
+}
+par(cex.axis=1, cex.lab=1, cex.main=1)
+
+
 
 
